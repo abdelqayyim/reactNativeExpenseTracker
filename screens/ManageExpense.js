5c592f1157dd3f299c5a3e74950f56cd751f99e8
abdelqayyim <abdelymaster@gmail.com>
2023-06-26 22:48:12 -0400

    Added error handlers

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -8,9 +8,11 @@ import { ExpensesContext } from '../store/expenses-context';
 import ExpenseForm from '../components/ManageExpense/ExpenseForm';
 import { deleteExpense, storeExpense, updateExpense } from '../util/http';
 import LoadingOverlay from '../components/UI/LoadingOverlay';
+import ErrorOverlay from '../components/UI/ErrorOverlay';
 
 function ManageExpense({ route, navigation }) {
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState();
   const expensesCtx = useContext(ExpensesContext);
 
   const editedExpenseId = route.params?.expenseId;
@@ -26,10 +28,15 @@ function ManageExpense({ route, navigation }) {
 
   async function deleteExpenseHandler() {
     setIsSubmitting(true);
-    await deleteExpense(editedExpenseId);
-    // setIsSubmitting(false);
-    expensesCtx.deleteExpense(editedExpenseId);
-    navigation.goBack();
+    try {
+      await deleteExpense(editedExpenseId);
+      expensesCtx.deleteExpense(editedExpenseId);
+      navigation.goBack();
+    }
+    catch (error) {
+      setError("Could not delete expense");
+      setIsSubmitting(false);
+    }
   }
 
   function cancelHandler() {
@@ -38,14 +45,27 @@ function ManageExpense({ route, navigation }) {
 
   async function confirmHandler(expenseData) {
     setIsSubmitting(true);
-    if (isEditing) {
-      expensesCtx.updateExpense(editedExpenseId, expenseData);
-      await updateExpense(editedExpenseId, expenseData);
-    } else {
-      const id = await storeExpense(expenseData);
-      expensesCtx.addExpense({...expenseData, id: id});
+    try {
+      if (isEditing) {
+        expensesCtx.updateExpense(editedExpenseId, expenseData);
+        await updateExpense(editedExpenseId, expenseData);
+      } else {
+        const id = await storeExpense(expenseData);
+        expensesCtx.addExpense({...expenseData, id: id});
+      }
+      navigation.goBack();
     }
-    navigation.goBack();
+    catch (error) {
+      setError('Could not save data - please try again later!');
+      setIsSubmitting(false);
+    }
+  }
+
+  function errorHandler() {
+    setError(null);
+  }
+  if (error & !isSubmitting) {
+    return <ErrorOverlay message={error} onConfirm={errorHandler}/>
   }
   if (isSubmitting) {
     return <LoadingOverlay/>
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -5,21 +5,35 @@ import { ExpensesContext } from '../store/expenses-context';
 import { getDateMinusDays } from '../util/date';
 import { fetchExpenses } from '../util/http';
 import LoadingOverlay from '../components/UI/LoadingOverlay';
+import ErrorOverlay from '../components/UI/ErrorOverlay';
 
 function RecentExpenses() {
-  const [isFectching, setIsFectching] = useState(true);
+  const [isFetching, setIsFectching] = useState(true);
+  const [error, setError] = useState();
   const expensesCtx = useContext(ExpensesContext);
 
   useEffect(() => {
     async function getExpenses() {
       setIsFectching(true);
-      const expenses = await fetchExpenses();
+      try {
+        const expenses = await fetchExpenses();
+        expensesCtx.setExpenses(expenses);
+      }
+      catch (error) {
+        setError('Could not fetch expeneses');
+      }
       setIsFectching(false);
-      expensesCtx.setExpenses(expenses);
     }
     getExpenses();
   }, []);
-  if (isFectching) {
+
+  function errorHandler() {
+    setError(null);
+  }
+  if (error & !isFetching) {
+    return <ErrorOverlay message={error} onConfirm={errorHandler}/>
+  }
+  if (isFetching) {
     return <LoadingOverlay />;
   }
 
